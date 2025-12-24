dba65872ae795c84ff0657ac1701714c81269e76
w1Lhm <wilhm0102@gmail.com>
2025-12-24 13:39:18 +0700

    initial commit

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,7 +4,6 @@ import "../css/app.css";
 import { createApp, h } from "vue";
 import { createInertiaApp } from "@inertiajs/vue3";
 import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
-import { ZiggyVue } from "../../vendor/tightenco/ziggy";
 
 // PrimeVue Configuration
 import PrimeVue from "primevue/config";
@@ -430,7 +429,6 @@ createInertiaApp({
     setup({ el, App, props, plugin }) {
         const app = createApp({ render: () => h(App, props) })
             .use(plugin)
-            .use(ZiggyVue)
             .use(PrimeVue, {
                 theme: {
                     preset: Aura,
